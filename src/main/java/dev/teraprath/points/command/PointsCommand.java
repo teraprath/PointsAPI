b862377184c44e361979298536cc898395439595
Terry <68700354+teraprath@users.noreply.github.com>
2023-04-28 16:25:06 +0200

    ADD Admin Command

---
+++ src/main/java/dev/teraprath/points/command/PointsCommand.java
@@ -0,0 +1,49 @@
+package dev.teraprath.points.command;
+
+import dev.teraprath.points.utils.PointsUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PointsCommand implements CommandExecutor, TabCompleter {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (args.length > 0) {
+            final OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
+            new PointsUtils().print(sender, player, args[0]);
+        } else {
+            if (sender instanceof Player) {
+                new PointsUtils().print(sender, (Player) sender, null);
+            } else {
+                sender.sendMessage("points <Player>");
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        String current = args[args.length - 1].toLowerCase();
+
+        if (args.length == 1) {
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                list.add(player.getName());
+            });
+        }
+
+        list.removeIf(s -> !s.toLowerCase().startsWith(current));
+        return list;
+    }
+
+}
